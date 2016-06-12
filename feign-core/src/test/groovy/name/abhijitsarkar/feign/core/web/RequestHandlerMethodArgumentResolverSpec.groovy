bbfdece81aa2184c5c0990aa6de05da80ad450c4
Abhijit Sarkar <sarkar.abhijit@outlook.com>
2016-06-12 00:58:36 -0700

    Preparing for release. Added copyright info.

--- feign-core/src/test/groovy/name/abhijitsarkar/feign/core/web/RequestHandlerMethodArgumentResolverSpec.groovy
+++ feign-core/src/test/groovy/name/abhijitsarkar/feign/core/web/RequestHandlerMethodArgumentResolverSpec.groovy
@@ -1,6 +1,24 @@
+/*
+ * Copyright (c) 2016, the original author or authors.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 of the License.
+ *  *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * A copy of the GNU General Public License accompanies this software,
+ * and is also available at http://www.gnu.org/licenses.
+ *
+ */
+
 package name.abhijitsarkar.feign.core.web
 
 import name.abhijitsarkar.feign.Request
+import org.springframework.core.MethodParameter
 import org.springframework.web.context.request.NativeWebRequest
 import spock.lang.Specification
 
@@ -35,5 +53,27 @@ class RequestHandlerMethodArgumentResolverSpec extends Specification {
         request.queryParams == httpServletRequest.parameterMap
         request.headers == ['a': 'b']
         request.body == 'body'
+    }
+
+    def "supports Request"() {
+        setup:
+        MethodParameter parameter = Mock(MethodParameter)
+        parameter.parameterType >> Request
+
+        expect:
+        new RequestHandlerMethodArgumentResolver().supportsParameter(parameter)
+    }
+
+    def "supports Request subclasses"() {
+        setup:
+        MethodParameter parameter = Mock(MethodParameter)
+        parameter.parameterType >> TestRequest
+
+        expect:
+        new RequestHandlerMethodArgumentResolver().supportsParameter(parameter)
+    }
+
+    private static final class TestRequest extends Request {
+
     }
 }
